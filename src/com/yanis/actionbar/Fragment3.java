ae840dffaa8ffd1af6e30584fb8d5f4c3d5d287e
YeXiaoChao <yechao@ycdoit.com>
2015-02-27 00:52:22 +0800

    update

---
+++ src/com/yanis/actionbar/Fragment3.java
@@ -0,0 +1,15 @@
+package com.yanis.actionbar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment3 extends android.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment3, container, false);
+    }
+}
--- src/com/yanis/actionbar/TabActivity.java
+++ src/com/yanis/actionbar/TabActivity.java
@@ -1,6 +1,7 @@
 package com.yanis.actionbar;
 
 import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,33 +10,62 @@ import android.support.v4.app.TaskStackBuilder;
 import android.view.MenuItem;
 
 public class TabActivity extends Activity {
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tab);
 
-	ActionBar actionBar = getActionBar();
-	actionBar.setDisplayHomeAsUpEnabled(true);
-}
-
-@Override
-public boolean onOptionsItemSelected(MenuItem item) {
-	switch (item.getItemId()) {
-	case android.R.id.home:
-		  Intent upIntent = NavUtils.getParentActivityIntent(this);  
-	        if (NavUtils.shouldUpRecreateTask(this, upIntent)) {  
-	            TaskStackBuilder.create(this)  
-	                    .addNextIntentWithParentStack(upIntent)  
-	                    .startActivities();  
-	        } else {  
-	            upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);  
-	            NavUtils.navigateUpTo(this, upIntent);  
-	        }  
-	        return true;  
-	default:
-		return super.onOptionsItemSelected(item);
+		initView();
 	}
-}
 
+	private void initView() {
+		// 提示getActionBar方法一定在setContentView后面
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// 添加Tab选项
+		Tab tab = actionBar
+				.newTab()
+				.setText("澳门风云2")
+				.setTabListener(
+						new TabListener<Fragment1>(this, "film1",
+								Fragment1.class));
+		actionBar.addTab(tab);
+
+		tab = actionBar
+				.newTab()
+				.setText("五十度灰")
+				.setTabListener(
+						new TabListener<Fragment2>(this, "film2",
+								Fragment2.class));
+		actionBar.addTab(tab);
+		tab = actionBar
+				.newTab()
+				.setText("爸爸去哪儿2")
+				.setTabListener(
+						new TabListener<Fragment3>(this, "film3",
+								Fragment3.class));
+		actionBar.addTab(tab);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent upIntent = NavUtils.getParentActivityIntent(this);
+			if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+				TaskStackBuilder.create(this)
+						.addNextIntentWithParentStack(upIntent)
+						.startActivities();
+			} else {
+				upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				NavUtils.navigateUpTo(this, upIntent);
+			}
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
 }
